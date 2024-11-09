501e9841c15c84361f3334a78dee3b24318152ee
loveandpower <allisonwellingtonbrown@gmail.com>
2024-11-08 19:58:37 -0500

    Create HerbiboarConfig.java

---
+++ HerbiboarConfig.java
@@ -0,0 +1,7 @@
+public class HerbiboarConfig {
+    public static final int MIN_HUNTER_LEVEL = 80;
+    public static final int MIN_HERBLORE_LEVEL = 31;
+    public static final double CATCH_CHANCE_BASE = 0.7;
+    public static final double CATCH_CHANCE_MAX = 0.95;
+    // Other configurable parameters
+}
