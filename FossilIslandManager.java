7bf68d818215890d25017c5f7a3d18b3c0dfa000
loveandpower <allisonwellingtonbrown@gmail.com>
2024-11-08 20:59:41 -0500

    Update FossilIslandManager.java
    Added coordinates

--- FossilIslandManager.java
+++ FossilIslandManager.java
@@ -1,6 +1,12 @@
 public class FossilIslandManager {
+    public static final int HERBIBOAR_REGION_ID = 14652; // Actual OSRS region ID
+
+    public static boolean isInHerbiboarArea(int x, int y, int plane) {
+        return x >= 3680 && x <= 3711 && y >= 3871 && y <= 3903 && plane == 0;
+    }
+
     public static boolean isInHerbiboarArea(Coordinate location) {
-        // Check if the given coordinate is within the Herbiboar hunting area
+        return isInHerbiboarArea(location.getX(), location.getY(), location.getPlane());
     }
 
     // Other Fossil Island related methods
