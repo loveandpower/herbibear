8298ebeb62721710ed1fd7529492f04262d9b6bd
loveandpower <allisonwellingtonbrown@gmail.com>
2024-11-08 20:43:48 -0500

    Create HerbiboarTrait.java

---
+++ HerbiboarTrait.java
@@ -0,0 +1,11 @@
+public class HerbiboarTrait {
+    private final String name;
+    private final double catchDifficultyModifier;
+
+    public HerbiboarTrait(String name, double catchDifficultyModifier) {
+        this.name = name;
+        this.catchDifficultyModifier = catchDifficultyModifier;
+    }
+
+    // Getters and methods to apply trait effects
+}
