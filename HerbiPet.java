bee896549ccd5f9b07afa92e43e8dad65ae2206e
loveandpower <allisonwellingtonbrown@gmail.com>
2024-11-08 21:03:16 -0500

    Update HerbiPet.java
    Added extra statements to correlate with all OSRS Pet rules. Will need to review any current pet methods on Kronos

--- HerbiPet.java
+++ HerbiPet.java
@@ -2,12 +2,20 @@ public class HerbiPet {
     private static final int HERBI_PET_ID = 21509;
     private static final double PET_CHANCE = 1.0 / 6500;
 
-    public static boolean rollForPet(Player player) {
+    public static void rollForPet(Player player) {
         if (Math.random() < PET_CHANCE) {
-            player.getInventory().add(HERBI_PET_ID, 1);
-            player.sendMessage("You have a funny feeling you're being followed...");
-            return true;
+            if (player.getInventory().contains(HERBI_PET_ID) || player.getBank().contains(HERBI_PET_ID)) {
+                player.sendMessage("You have a funny feeling like you would have been followed...");
+            } else if (player.hasFollower()) {
+                player.sendMessage("You have a funny feeling like you're being followed, but you already have a follower.");
+                player.getBank().add(HERBI_PET_ID, 1);
+            } else if (player.getInventory().hasSpace()) {
+                player.getInventory().add(HERBI_PET_ID, 1);
+                player.sendMessage("You have a funny feeling you're being followed...");
+            } else {
+                player.getBank().add(HERBI_PET_ID, 1);
+                player.sendMessage("You feel something weird sneaking into your bank account.");
+            }
         }
-        return false;
     }
 }
