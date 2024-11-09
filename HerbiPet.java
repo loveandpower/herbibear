a53b1ae2ec124ccf1b095395e765a903191a91f4
loveandpower <allisonwellingtonbrown@gmail.com>
2024-11-09 07:09:04 -0500

    Update HerbiPet.java
    Added following: 
    -Check for any current pet
    -Check for a pet currently following
    -Check for Banking handling in situation with full inventory

--- HerbiPet.java
+++ HerbiPet.java
@@ -1,18 +1,29 @@
+package io.ruin.model.content;
+
+import io.ruin.model.entity.player.Player;
+
 public class HerbiPet {
-    private static final int HERBI_PET_ID = 21509;
-    private static final double PET_CHANCE = 1.0 / 6500;
+    private static final int HERBI_PET_ID = 21509; // ID for the Herbi pet
+    private static final double PET_CHANCE = 1.0 / 6500; // Chance to receive the pet
 
     public static void rollForPet(Player player) {
         if (Math.random() < PET_CHANCE) {
+            // Check if the player already has the pet in their inventory or bank
             if (player.getInventory().contains(HERBI_PET_ID) || player.getBank().contains(HERBI_PET_ID)) {
                 player.sendMessage("You have a funny feeling like you would have been followed...");
-            } else if (player.hasFollower()) {
+            } 
+            // Check if the player already has a follower
+            else if (player.hasFollower()) {
                 player.sendMessage("You have a funny feeling like you're being followed, but you already have a follower.");
-                player.getBank().add(HERBI_PET_ID, 1);
-            } else if (player.getInventory().hasSpace()) {
+                player.getBank().add(HERBI_PET_ID, 1); // Send to bank if they can't hold it
+            } 
+            // If they have space in their inventory
+            else if (player.getInventory().hasSpace()) {
                 player.getInventory().add(HERBI_PET_ID, 1);
                 player.sendMessage("You have a funny feeling you're being followed...");
-            } else {
+            } 
+            // If inventory is full, send to bank
+            else {
                 player.getBank().add(HERBI_PET_ID, 1);
                 player.sendMessage("You feel something weird sneaking into your bank account.");
             }
