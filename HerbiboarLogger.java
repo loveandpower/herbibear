20d2187e14aa132f384311e0eab11bb237a594ad
loveandpower <allisonwellingtonbrown@gmail.com>
2024-11-08 19:57:49 -0500

    Create HerbiboarLogger.java

---
+++ HerbiboarLogger.java
@@ -0,0 +1,9 @@
+public class HerbiboarLogger {
+    public static void log(String message) {
+        // Implement logging
+    }
+
+    public static void logError(String message, Exception e) {
+        // Implement error logging
+    }
+}
