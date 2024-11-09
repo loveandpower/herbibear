dde996071b9634264bc2f46b248c61bb4d38c347
loveandpower <allisonwellingtonbrown@gmail.com>
2024-11-09 07:30:52 -0500

    Update HerbiboarFootprint.java
    Converted PI code to Kronos Position class 
    Added Spawn + Remove footprint methods
    Despawn Logic (James you may need to modify this to fit more with Kronos)

--- HerbiboarFootprint.java
+++ HerbiboarFootprint.java
@@ -1,11 +1,15 @@
-public class HerbiboarFootprint {
-    private static final int FOOTPRINT_OBJECT_ID = 30918;
-    private static final int DESPAWN_TICKS = 50; // Adjust as needed
+package io.ruin.model.content;
 
-    private final Coordinate location;
+import io.ruin.model.map.Position;
+
+public class HerbiboarFootprint {
+    private static final int FOOTPRINT_OBJECT_ID = 30918; // Object ID for the footprint
+    private static final int DESPAWN_TICKS = 50; // Time in ticks before the footprint despawns
+
+    private final Position location; // Use Position instead of Coordinate
     private int despawnTimer;
 
-    public HerbiboarFootprint(Coordinate location) {
+    public HerbiboarFootprint(Position location) {
         this.location = location;
         this.despawnTimer = DESPAWN_TICKS;
     }
@@ -18,5 +22,17 @@ public class HerbiboarFootprint {
 
     public boolean shouldDespawn() {
         return despawnTimer == 0;
+    }
+
+    public Position getLocation() {
+        return location;
+    }
+
+    public void spawnFootprint() {
+        GameObject.spawn(FOOTPRINT_OBJECT_ID, location); // Spawn the footprint at the location
+    }
+
+    public void removeFootprint() {
+        GameObject.remove(FOOTPRINT_OBJECT_ID, location); // Remove the footprint from the game world
     }
 }
