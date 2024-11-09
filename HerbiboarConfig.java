0b50a0af3768ea9bad8af80a4bca426b9a185a91
loveandpower <allisonwellingtonbrown@gmail.com>
2024-11-09 07:23:50 -0500

    Update HerbiboarConfig.java
    Added config for max herbs with and without magic secateurs

--- HerbiboarConfig.java
+++ HerbiboarConfig.java
@@ -1,7 +1,11 @@
+package io.ruin.model.content;
+
 public class HerbiboarConfig {
-    public static final int MIN_HUNTER_LEVEL = 80;
-    public static final int MIN_HERBLORE_LEVEL = 31;
-    public static final double CATCH_CHANCE_BASE = 0.7;
-    public static final double CATCH_CHANCE_MAX = 0.95;
-    // Other configurable parameters
+    public static final int MIN_HUNTER_LEVEL = 80; // Minimum Hunter level required to track Herbiboar
+    public static final int MIN_HERBLORE_LEVEL = 31; // Minimum Herblore level required to harvest herbs
+    public static final double CATCH_CHANCE_BASE = 0.7; // Base catch chance for the Herbiboar
+    public static final double CATCH_CHANCE_MAX = 0.95; // Maximum catch chance at high Hunter levels
+    public static final int MAX_HERB_COUNT_WITH_SECATEURS = 3; // Maximum herbs harvested with Magic Secateurs
+    public static final int BASE_HERB_COUNT = 1; // Base number of herbs harvested without Magic Secateurs
+    
 }
