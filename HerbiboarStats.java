20e7f6fb8a837726ebd95ca957a0ff24596e34a9
loveandpower <allisonwellingtonbrown@gmail.com>
2024-11-08 19:55:20 -0500

    Create HerbiboarStats.java

---
+++ HerbiboarStats.java
@@ -0,0 +1,7 @@
+public class HerbiboarStats {
+    private int totalCaught;
+    private int failedAttempts;
+    private Map<String, Integer> herbsCollected;
+
+    // Methods to update and retrieve stats
+}
