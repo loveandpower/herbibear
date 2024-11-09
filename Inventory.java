f8fb836a32ea8aa3b30ce7e42de9fd2af031c89d
loveandpower <allisonwellingtonbrown@gmail.com>
2024-11-08 19:47:06 -0500

    Create Inventory.java
    Initial commit. James probably has all of this written hella better. Probably has a custom system so he will have to convert it. Ripped from my old PI.

---
+++ Inventory.java
@@ -0,0 +1,10 @@
+public class Inventory {
+    public boolean contains(String itemName) {
+        // Code to check if inventory contains an item
+        return false;
+    }
+
+    public void add(String itemName, int amount) {
+        // Code to add an item to the inventory
+    }
+}
