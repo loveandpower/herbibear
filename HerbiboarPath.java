cf88bf2805fd64fb5bba00bff08dc3bc4329a20b
loveandpower <allisonwellingtonbrown@gmail.com>
2024-11-08 19:52:42 -0500

    Create HerbiboarPath.java

---
+++ HerbiboarPath.java
@@ -0,0 +1,9 @@
+public class HerbiboarPath {
+    private final List<TrackingSpot> spots;
+
+    public HerbiboarPath(List<TrackingSpot> spots) {
+        this.spots = spots;
+    }
+
+    // Methods to get next spot, check if path is complete, etc.
+}
