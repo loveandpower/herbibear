db1a3b52342b7adeb8d84782f149855278832788
loveandpower <allisonwellingtonbrown@gmail.com>
2024-11-09 11:18:58 -0500

    Update HerbiboarTrait.java
    Modified to reflect catchChance to be as closely relative to OSRS Mechanics as wiki and other guides explain. Note There isn't a large amount of information out there and this is my interpretation.

--- HerbiboarTrait.java
+++ HerbiboarTrait.java
@@ -1,11 +1,54 @@
-public class HerbiboarTrait {
-    private final String name;
-    private final double catchDifficultyModifier;
+package io.ruin.model.content;
 
-    public HerbiboarTrait(String name, double catchDifficultyModifier) {
+import java.util.Random;
+
+public class HerbiboarTrait {
+    private static final Random RANDOM = new Random();
+
+    private final String name;
+    private final int minSteps;
+    private final int maxSteps;
+
+    public static final HerbiboarTrait STANDARD = new HerbiboarTrait("Standard", 3, 5);
+    public static final HerbiboarTrait QUICK = new HerbiboarTrait("Quick", 4, 6);
+    public static final HerbiboarTrait SLUGGISH = new HerbiboarTrait("Sluggish", 2, 4);
+
+    private HerbiboarTrait(String name, int minSteps, int maxSteps) {
         this.name = name;
-        this.catchDifficultyModifier = catchDifficultyModifier;
+        this.minSteps = minSteps;
+        this.maxSteps = maxSteps;
     }
 
-    // Getters and methods to apply trait effects
+    public String getName() {
+        return name;
+    }
+
+    public int getSteps() {
+        return RANDOM.nextInt(maxSteps - minSteps + 1) + minSteps;
+    }
+
+    public static HerbiboarTrait getRandomTrait() {
+        double roll = RANDOM.nextDouble();
+        if (roll < 0.2) return QUICK;
+        if (roll < 0.4) return SLUGGISH;
+        return STANDARD;
+    }
+
+    public static double calculateCatchChance(int hunterLevel) {
+        // Base catch chance starts at 65% at level 80
+        double baseChance = 0.65;
+        
+        // Increase by 1% for every level above 80, up to 95% at level 99
+        if (hunterLevel > 80) {
+            baseChance += Math.min((hunterLevel - 80) * 0.01, 0.30);
+        }
+        
+        return Math.min(baseChance, 0.95);
+    }
+
+    public boolean attemptCatch(Player player) {
+        int hunterLevel = player.getStats().get(StatType.Hunter).currentLevel;
+        double catchChance = calculateCatchChance(hunterLevel);
+        return RANDOM.nextDouble() < catchChance;
+    }
 }
