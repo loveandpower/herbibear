b240f8a258253a5d3d3b8bbd93d73c40d3353e9f
loveandpower <allisonwellingtonbrown@gmail.com>
2024-11-08 20:00:48 -0500

    Create FossilIslandManager.java

---
+++ FossilIslandManager.java
@@ -0,0 +1,7 @@
+public class FossilIslandManager {
+    public static boolean isInHerbiboarArea(Coordinate location) {
+        // Check if the given coordinate is within the Herbiboar hunting area
+    }
+
+    // Other Fossil Island related methods
+}
