22a5cb0138d1dc9f32035c93831acd4b0a71cd65
loveandpower <allisonwellingtonbrown@gmail.com>
2024-11-08 19:45:46 -0500

    Create Skills.java
    james probably already has this shit but just the basics ripped from my old PI.

---
+++ Skills.java
@@ -0,0 +1,13 @@
+public class Skills {
+    public static final int HUNTER = 21;
+    public static final int HERBLORE = 15;
+
+    public int getLevel(int skillId) {
+        // Code to get skill level
+        return 0;
+    }
+
+    public void addXp(int skillId, int xp) {
+        // Code to add XP to a skill
+    }
+}
